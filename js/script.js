a88b9762b112e63dab12f37d6afb3d5bbad88f13
Eng.Tala <talabts33@gmail.com>
2026-02-03 20:39:00 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 
 
-//   const menuBtn = document.getElementById("menuBtn");
-//   const sideMenu = document.getElementById("sideMenu");
-//   const overlay = document.getElementById("overlay");
-//   const links = document.querySelectorAll(".menu-link");
-
-// function toggleMenu() {
-//   sideMenu.classList.toggle("-translate-x-full");
-//   overlay.classList.toggle("hidden");
-
-//   const isOpen = !sideMenu.classList.contains("-translate-x-full");
-//   menuBtn.textContent = isOpen ? "✕" : "☰";
-// }
-
-//   menuBtn.addEventListener("click", toggleMenu);
-//   overlay.addEventListener("click", toggleMenu);
-
-//   links.forEach(link =>
-//     link.addEventListener("click", toggleMenu)
-//   );
   const menuBtn = document.getElementById("menuBtn");
   const sideMenu = document.getElementById("sideMenu");
-  menuBtn.onclick=()=>{
+  const overlay = document.getElementById("overlay");
+  const links = document.querySelectorAll(".menu-link");
+
+function toggleMenu() {
   sideMenu.classList.toggle("-translate-x-full");
-   overlay.classList.toggle("hidden");
+  overlay.classList.toggle("hidden");
+
+  const isOpen = !sideMenu.classList.contains("-translate-x-full");
+  menuBtn.textContent = isOpen ? "✕" : "☰";
 }
+
+  menuBtn.addEventListener("click", toggleMenu);
+  overlay.addEventListener("click", toggleMenu);
+
+  links.forEach(link =>
+    link.addEventListener("click", toggleMenu)
+  );
+//   const menuBtn = document.getElementById("menuBtn");
+//   const sideMenu = document.getElementById("sideMenu");
+//   menuBtn.onclick=()=>{
+//   sideMenu.classList.toggle("-translate-x-full");
+//    overlay.classList.toggle("hidden");
+// }
   
